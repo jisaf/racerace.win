479a521a67ec63e47d9b324c5ab38d3547e88ba4
Katherine Mello <katherine.mello8@gmail.com>
2016-05-07 18:35:30 -0400

    socket factory

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,21 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: function($scope, socket){
+        	let players = 0;
+        	socket.on('playerconnected', function(){
+        		console.log('playerconnected event')
+        		players++;
+        		console.log('num players:', players)
+        	})
+        	socket.on('connect', function(){
+        		socket.emit('playerconnected')
+        		players++;
+        		console.log('num players:', players)
+        	});
+
+        	socket.on('disconnect', function(){console.log('disconnected')})
+        }
     });
 });
