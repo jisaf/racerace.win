b1088452c84147fac4942d3bc0a5873d8f5cc3a1
Laura Weiner <lauraweiner@icloud.com>
2016-05-07 21:42:10 -0400

    put broken sockets back in

--- browser/js/player/player.controller.js
+++ browser/js/player/player.controller.js
@@ -7,16 +7,16 @@ app.controller('PlayerCtrl', function($scope) {
     const MIN_SAMPLES = 0; // will be initialized when AudioContext is created.
     const buflen = 1024;
     const buf = new Float32Array(buflen);
-    // let isPlaying = false;
+    let isPlaying = false;
 
-    // socket.on('raceStart', function() {
-    // 	isPlaying = true;
+    socket.on('raceStart', function() {
+    	isPlaying = true;
         toggleLiveInput();
-    // })
+    })
 
-    // socket.on('raceEnd', function() {
-    // 	isPlaying = false;
-    // })
+    socket.on('raceEnd', function() {
+    	isPlaying = false;
+    })
     //ALL CREDIT REGARDING THE LOGIC SURROUNDING PITCH SHALL GO TO https://github.com/cwilso/PitchDetect
 
     if (window.DeviceOrientationEvent) {
@@ -32,7 +32,7 @@ app.controller('PlayerCtrl', function($scope) {
             let averageOrientation = deviceOrientation.reduce(function(prev, curr){
                 return prev + curr
             }, 0)/ deviceOrientation.length
-            // socket.emit('xOrientationChange', {deviceXOrientation: averageOrientation})
+            socket.emit('xOrientationChange', {deviceXOrientation: averageOrientation})
         }
 
 
@@ -154,9 +154,9 @@ app.controller('PlayerCtrl', function($scope) {
     const lastFivePitches = [0, 0, 0, 0, 0];
     let i =0;
     function updatePitch(time) {
-    	// if(!isPlaying){
-    	// 	return false;
-    	// }
+    	if(!isPlaying){
+    		return false;
+    	}
         analyser.getFloatTimeDomainData(buf);
         const ac = autoCorrelate(buf, audioContext.sampleRate);
 
@@ -166,7 +166,6 @@ app.controller('PlayerCtrl', function($scope) {
             //this is equivalent to slamming on the brake -- velocity goes to 0
             velocity = 0;
         } else {
-            // console.log('in else')
             if (Math.abs(ac - lastFivePitches[lastFivePitches.length-1]) > 50){
                 lastFivePitches.shift();
                 lastFivePitches.push(ac);
@@ -181,15 +180,11 @@ app.controller('PlayerCtrl', function($scope) {
                 }else {
                 	velocity = averagePitch/350;
                 }
-                // console.log('veloc',velocity)
-                $scope.velocity = velocity;
-                // console.log('scopeee',$scope.velocity)
                 $scope.prettyVelocity = Math.round(velocity * 230)
-                console.log('pv',$scope.prettyVelocity)
                 $scope.$apply();
             }
         }
-        // socket.emit('velocity', {velocity: velocity});
+        socket.emit('velocity', {velocity: velocity});
 
 
 
