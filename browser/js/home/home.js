327f183b0500eb49fae2cb95c317531e5c17827f
Iris <icc.lany@gmail.com>
2016-05-08 02:02:01 -0400

    good change

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -2,6 +2,6 @@ app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         controller: 'HomeCtrl',
-        templateUrl: '/home.html'
+        templateUrl: 'js/home/home.html'
     });
 });
